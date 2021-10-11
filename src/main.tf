3d2f98f03625909d82ffb0e8a0c96e32caece3f3
Dakota Riley <dakotariley2@gmail.com>
2021-10-11 03:52:25 +0000

    fix: adjust egress property

--- src/main.tf
+++ src/main.tf
@@ -36,15 +36,14 @@ resource "aws_security_group" "block_all_inbound" {
   description = "Blocks all inbound traffic while allowing outbound"
   vpc_id      = aws_vpc.demo_vpc.id
 
-  egress = [
-    {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-    }
-  ]
+  egress = {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
 
   tags = local.common_tags
 }
