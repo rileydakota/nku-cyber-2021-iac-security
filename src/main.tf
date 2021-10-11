c39b7e4aecfe8b784b2a1543d909479794e4d7f0
Dakota Riley <dakotariley2@gmail.com>
2021-10-11 04:29:00 +0000

    fix: add all required attributes

--- src/main.tf
+++ src/main.tf
@@ -73,7 +73,17 @@ resource "aws_route_table" "public_route_table" {
   route = [
     {
       cidr_block = "0.0.0.0/0"
-      gateway_id = aws_internet_gateway.igw.id 
+      gateway_id = aws_internet_gateway.igw.id
+      carrier_gateway_id = ""
+      destination_prefix_list_id = ""
+      egress_only_gateway_id = ""
+      instance_id = ""
+      local_gateway_id = ""
+      nat_gateway_id =  ""
+      network_interface_id = ""
+      transit_gateway_id = ""
+      vpc_endpoint_id = ""
+      vpc_peering_connection_id = ""
     }
   ]
 
