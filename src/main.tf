07b301c6e34fa7bc670387ad3ffd19267452a5a5
Dakota Riley <dakotariley2@gmail.com>
2021-10-14 22:39:17 +0000

    feat: reconfigure sg for bad things

--- src/main.tf
+++ src/main.tf
@@ -35,6 +35,17 @@ resource "aws_security_group" "block_all_inbound" {
   name        = "block_all_inbound"
   description = "Blocks all inbound traffic while allowing outbound"
   vpc_id      = aws_vpc.demo_vpc.id
+  ingress = [{
+    description      = "very insecure rule"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+    prefix_list_ids  = []
+    security_groups  = []
+    self             = false
+  }]
 
   egress = [{
     description      = "allow all outbound"
